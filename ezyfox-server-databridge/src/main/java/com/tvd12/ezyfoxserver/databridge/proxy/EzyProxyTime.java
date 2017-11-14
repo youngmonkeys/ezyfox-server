b177b3d9d3830539c635ac824e3b916f16ddabba
Dung Ta Van <itprono3@gmail.com>
2017-11-14 11:14:44 +0700

    update databridge

---
+++ ezyfox-server-databridge/src/main/java/com/tvd12/ezyfoxserver/databridge/proxy/EzyProxyTime.java
@@ -0,0 +1,19 @@
+package com.tvd12.ezyfoxserver.databridge.proxy;
+
+import java.util.Date;
+
+import com.tvd12.ezyfoxserver.io.EzyDates;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class EzyProxyTime {
+
+	@Getter
+	private final long millis;
+	
+	public String getTimestamp() {
+		return EzyDates.format(new Date(millis), "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+	}
+}

4b68b153550f43dc5c32fdf69c96e2e404415328
Dung Ta Van <itprono3@gmail.com>
2017-11-21 21:31:35 +0700

    update binding

---
+++ ezyfox-server-binding/src/main/java/com/tvd12/ezyfoxserver/binding/EzyMarshallerAware.java
@@ -0,0 +1,7 @@
+package com.tvd12.ezyfoxserver.binding;
+
+public interface EzyMarshallerAware {
+
+	void setMarshaller(EzyMarshaller marshaller);
+	
+}
---
+++ ezyfox-server-binding/src/main/java/com/tvd12/ezyfoxserver/binding/EzyUnmarshallerAware.java
@@ -0,0 +1,7 @@
+package com.tvd12.ezyfoxserver.binding;
+
+public interface EzyUnmarshallerAware {
+
+	void setUnmarshaller(EzyUnmarshaller unmarshaller);
+	
+}

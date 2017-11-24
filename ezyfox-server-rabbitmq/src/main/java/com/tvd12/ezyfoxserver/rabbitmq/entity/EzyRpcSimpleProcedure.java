fdbb5df4416ed371bc1ed8f03185baefdd6d758b
Dung Ta Van <itprono3@gmail.com>
2017-11-24 16:15:17 +0700

    update rpc procedure

--- ezyfox-server-rabbitmq/src/main/java/com/tvd12/ezyfoxserver/rabbitmq/entity/EzyRpcSimpleProcedure.java
+++ ezyfox-server-rabbitmq/src/main/java/com/tvd12/ezyfoxserver/rabbitmq/entity/EzyRpcSimpleProcedure.java
@@ -1,14 +1,11 @@
 package com.tvd12.ezyfoxserver.rabbitmq.entity;
 
-import org.msgpack.annotation.Message;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@Message
 @ToString(callSuper = true)
 public class EzyRpcSimpleProcedure extends EzyRpcAbstractProcedure {
 

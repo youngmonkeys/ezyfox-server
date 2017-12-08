8ceda194af64e3197f2c6229047390a220fee1cb
Dung Ta Van <itprono3@gmail.com>
2017-12-08 13:41:11 +0700

    update entities

---
+++ ezyfox-server-common/src/main/java/com/tvd12/ezyfoxserver/util/EzyEntityArrays.java
@@ -0,0 +1,21 @@
+package com.tvd12.ezyfoxserver.util;
+
+import java.util.List;
+
+import com.tvd12.ezyfoxserver.builder.EzyArrayBuilder;
+import com.tvd12.ezyfoxserver.entity.EzyArray;
+import com.tvd12.ezyfoxserver.factory.EzyEntityFactory;
+
+@SuppressWarnings("rawtypes")
+public final class EzyEntityArrays {
+
+	private EzyEntityArrays() {
+	}
+	
+	public static EzyArray newArray(List list) {
+		return EzyEntityFactory.create(EzyArrayBuilder.class)
+				.append(list)
+				.build();
+	}
+	
+}

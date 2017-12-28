58baaef51264d740556c31d2e06bf687f6436fb4
Dung Ta Van <itprono3@gmail.com>
2017-12-28 14:20:34 +0700

    refactor

--- ezyfox-server-core/src/main/java/com/tvd12/ezyfoxserver/EzyRunner.java
+++ ezyfox-server-core/src/main/java/com/tvd12/ezyfoxserver/EzyRunner.java
@@ -10,22 +10,22 @@ package com.tvd12.ezyfoxserver;
 public abstract class EzyRunner {
     
     public void run(String[] args) throws Exception {
-    	validateArguments(args);
-    	startSystem(args[0]);
+        validateArguments(args);
+    	    startSystem(args[0]);
     }
     
     protected void validateArguments(String[] args) {
-    	if(args.length == 0)
-    		throw new IllegalStateException("must specific config.properties file");
+    	    if(args.length == 0)
+    	        throw new IllegalStateException("must specific config.properties file");
     }
     
     protected void startSystem(String configFile) throws Exception {
-    	EzyStarter starter = newStarter(configFile);
-    	starter.start();
+        EzyStarter starter = newStarter(configFile);
+    	    starter.start();
     }
     
     protected EzyStarter newStarter(String configFile) {
-    	return newStarterBuilder().configFile(configFile).build();
+    	    return newStarterBuilder().configFile(configFile).build();
     }
     
     protected abstract EzyStarter.Builder<?> newStarterBuilder();

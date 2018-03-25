aa3142d45d84547b8dca17c3eeeab3dd28b05679
Dung Ta Van <itprono3@gmail.com>
2018-03-25 17:26:19 +0700

    update nio: common socket boostrap

--- ezyfox-server-nio/src/main/java/com/tvd12/ezyfoxserver/nio/EzyWebSocketServerBootstrap.java
+++ ezyfox-server-nio/src/main/java/com/tvd12/ezyfoxserver/nio/EzyWebSocketServerBootstrap.java
@@ -6,41 +6,22 @@ import javax.net.ssl.SSLContext;
 
 import org.eclipse.jetty.server.Server;
 
-import com.tvd12.ezyfoxserver.builder.EzyBuilder;
-import com.tvd12.ezyfoxserver.context.EzyServerContext;
 import com.tvd12.ezyfoxserver.nio.builder.impl.EzyWebSocketSecureServerCreator;
 import com.tvd12.ezyfoxserver.nio.builder.impl.EzyWebSocketServerCreator;
 import com.tvd12.ezyfoxserver.nio.constant.EzyNioThreadPoolSizes;
 import com.tvd12.ezyfoxserver.nio.websocket.EzyWsWritingLoopHandler;
-import com.tvd12.ezyfoxserver.nio.wrapper.EzyHandlerGroupManager;
-import com.tvd12.ezyfoxserver.nio.wrapper.EzyNioSessionManager;
-import com.tvd12.ezyfoxserver.setting.EzySessionManagementSetting;
-import com.tvd12.ezyfoxserver.setting.EzySettings;
 import com.tvd12.ezyfoxserver.setting.EzyWebSocketSetting;
-import com.tvd12.ezyfoxserver.socket.EzySessionTicketsQueue;
 import com.tvd12.ezyfoxserver.socket.EzySocketEventLoopHandler;
 import com.tvd12.ezyfoxserver.socket.EzySocketWriter;
-import com.tvd12.ezyfoxserver.util.EzyDestroyable;
-import com.tvd12.ezyfoxserver.util.EzyStartable;
-import com.tvd12.ezyfoxserver.wrapper.EzyManagers;
-import com.tvd12.ezyfoxserver.wrapper.EzySessionManager;
 
-public class EzyWebSocketServerBootstrap 
-		implements EzyStartable, EzyDestroyable {
+public class EzyWebSocketServerBootstrap extends EzyAbstractSocketServerBootstrap {
 
 	private Server server;
 	private SSLContext sslContext;
-	private EzySocketEventLoopHandler writingLoopHandler;
-	
-	private EzyServerContext serverContext;
-	private EzyHandlerGroupManager handlerGroupManager;
-	private EzySessionTicketsQueue sessionTicketsQueue;
 	
 	public EzyWebSocketServerBootstrap(Builder builder) {
+		super(builder);
 		this.sslContext = builder.sslContext;
-		this.serverContext = builder.serverContext;
-		this.handlerGroupManager = builder.handlerGroupManager;
-		this.sessionTicketsQueue = builder.sessionTicketsQueue;
 	}
 	
 	@Override
@@ -96,51 +77,17 @@ public class EzyWebSocketServerBootstrap
 		return getServerSettings().getWebsocket();
 	}
 	
-	private EzySessionManagementSetting getSessionSetting() {
-		return getServerSettings().getSessionManagement();
-	}
-	
-	private EzySettings getServerSettings() {
-		return serverContext.getServer().getSettings();
-	}
-	
-	private EzyManagers getServerManagers() {
-		return serverContext.getServer().getManagers();
-	}
-	
-	private EzyNioSessionManager getSessionManager() {
-		return (EzyNioSessionManager) 
-				getServerManagers().getManager(EzySessionManager.class);
-	}
-	
 	public static Builder builder() {
 		return new Builder();
 	}
 	
-	public static class Builder implements EzyBuilder<EzyWebSocketServerBootstrap> {
+	public static class Builder 
+			extends EzyAbstractSocketServerBootstrap.Builder<Builder, EzyWebSocketServerBootstrap> {
 
 		private SSLContext sslContext;
-		private EzyServerContext serverContext;
-		private EzyHandlerGroupManager handlerGroupManager;
-		private EzySessionTicketsQueue sessionTicketsQueue;
 		
 		public Builder sslContext(SSLContext sslContext) {
 			this.sslContext = sslContext;
-			return this;
-		}
-		
-		public Builder serverContext(EzyServerContext context) {
-			this.serverContext = context;
-			return this;
-		}
-		
-		public Builder handlerGroupManager(EzyHandlerGroupManager manager) {
-			this.handlerGroupManager = manager;
-			return this;
-		}
-		
-		public Builder sessionTicketsQueue(EzySessionTicketsQueue queue) {
-			this.sessionTicketsQueue = queue;
 			return this;
 		}
 		

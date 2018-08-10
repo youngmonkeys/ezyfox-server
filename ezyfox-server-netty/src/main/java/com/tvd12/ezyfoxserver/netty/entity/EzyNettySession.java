ee263f40718125b552e3283790b9f3fb1f384e84
Dung Ta Van <itprono3@gmail.com>
2018-08-10 19:10:57 +0700

    remote netty

--- ezyfox-server-netty/src/main/java/com/tvd12/ezyfoxserver/netty/entity/EzyNettySession.java
+++
@@ -1,6 +0,0 @@
-package com.tvd12.ezyfoxserver.netty.entity;
-
-import com.tvd12.ezyfoxserver.entity.EzySession;
-
-public interface EzyNettySession extends EzySession {
-}
--- ezyfox-server-netty/src/main/java/com/tvd12/ezyfoxserver/netty/wrapper/EzyHandlerGroupManagerAware.java
+++
@@ -1,7 +0,0 @@
-package com.tvd12.ezyfoxserver.netty.wrapper;
-
-public interface EzyHandlerGroupManagerAware {
-
-	void setHandlerGroupManager(EzyHandlerGroupManager manager);
-	
-}

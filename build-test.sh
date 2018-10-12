cf17815354f13855103d0b378dbe499ae2d6d276
Dung Ta Van <itprono3@gmail.com>
2018-10-12 22:58:36 +0700

    update build file

---
+++ build-test.sh
@@ -0,0 +1,10 @@
+mvn clean install
+mvn clean install -f ezyfox-server-core
+mvn clean install -f ezyfox-server-databridge
+mvn clean install -f ezyfox-server-webapi
+mvn clean install -f ezyfox-server-httpserver
+mvn clean install -f ezyfox-server-boot
+mvn clean install -f ezyfox-server-nio
+mvn clean install -f ezyfox-server-admintools
+mvn clean install -f ezyfox-server-niorunner
+mvn clean install -f ezyfox-server-support
--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-mvn clean install
-mvn clean install -f ezyfox-server-core
-mvn clean install -f ezyfox-server-databridge
-mvn clean install -f ezyfox-server-webapi
-mvn clean install -f ezyfox-server-httpserver
-mvn clean install -f ezyfox-server-boot
-mvn clean install -f ezyfox-server-nio
-mvn clean install -f ezyfox-server-admintools
-mvn clean install -f ezyfox-server-niorunner
-mvn clean install -f ezyfox-server-support
+mvn clean install -Dmaven.test.skip=true
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-core
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-databridge
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-webapi
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-httpserver
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-boot
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-nio
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-admintools
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-niorunner
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-support

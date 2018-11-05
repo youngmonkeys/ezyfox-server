17408a7f380b696ba31f2bd5a36d918fb2d402c4
Dung Ta Van <itprono3@gmail.com>
2018-11-05 22:52:22 +0700

    remove admin tool

--- ezyfox-server-admintools/src/main/java/com/tvd12/ezyfoxserver/admintools/service/EzyCPUService.java
+++
@@ -1,7 +0,0 @@
-package com.tvd12.ezyfoxserver.admintools.service;
-
-import com.tvd12.ezyfoxserver.admintools.data.EzyCpuPoint;
-
-public interface EzyCPUService {
-	EzyCpuPoint getCPUDetails();
-}
--- ezyfox-server-admintools/src/main/java/com/tvd12/ezyfoxserver/admintools/service/EzyNetWorkStatsService.java
+++
@@ -1,7 +0,0 @@
-package com.tvd12.ezyfoxserver.admintools.service;
-
-import com.tvd12.ezyfoxserver.admintools.data.EzyNetworkPoint;
-
-public interface EzyNetWorkStatsService {
-	EzyNetworkPoint getNetWorkStats();
-}

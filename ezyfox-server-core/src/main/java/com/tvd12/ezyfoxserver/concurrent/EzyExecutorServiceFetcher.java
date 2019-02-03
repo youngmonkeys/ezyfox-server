bf284aaee0c2e87d29283c80ce519aa8da4bebf2
Dung Ta Van <itprono3@gmail.com>
2019-02-03 21:55:16 +0700

    update context

---
+++ ezyfox-server-core/src/main/java/com/tvd12/ezyfoxserver/concurrent/EzyExecutorServiceFetcher.java
@@ -0,0 +1,9 @@
+package com.tvd12.ezyfoxserver.concurrent;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public interface EzyExecutorServiceFetcher {
+
+    ScheduledExecutorService getExecutorService();
+    
+}

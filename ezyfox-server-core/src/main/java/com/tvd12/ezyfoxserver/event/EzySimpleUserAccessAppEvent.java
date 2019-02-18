ea7b729b4939aca4bdd01d599ec36e24693844ce
Dung Ta Van <itprono3@gmail.com>
2019-02-18 12:00:36 +0700

    update core: update EzySimpleUserEvent - add ouput data

--- ezyfox-server-core/src/main/java/com/tvd12/ezyfoxserver/event/EzySimpleUserAccessAppEvent.java
+++ ezyfox-server-core/src/main/java/com/tvd12/ezyfoxserver/event/EzySimpleUserAccessAppEvent.java
@@ -1,12 +1,20 @@
 package com.tvd12.ezyfoxserver.event;
 
+import com.tvd12.ezyfox.entity.EzyArray;
+import com.tvd12.ezyfox.factory.EzyEntityFactory;
 import com.tvd12.ezyfoxserver.entity.EzyUser;
 
+import lombok.Getter;
+
+@Getter
 public class EzySimpleUserAccessAppEvent 
 		extends EzySimpleUserEvent 
 		implements EzyUserAccessAppEvent {
 
+    protected final EzyArray output;
+    
     public EzySimpleUserAccessAppEvent(EzyUser user) {
         super(user);
+        this.output = EzyEntityFactory.newArray();
     }
 }

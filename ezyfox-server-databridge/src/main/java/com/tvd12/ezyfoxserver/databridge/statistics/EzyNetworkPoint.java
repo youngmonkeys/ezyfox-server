eb431323f7c0baa83d1d6dda5c67824304b6d2d3
Dung Ta Van <itprono3@gmail.com>
2019-05-22 00:16:21 +0700

    update webapi and databridge

--- ezyfox-server-databridge/src/main/java/com/tvd12/ezyfoxserver/databridge/statistics/EzyNetworkPoint.java
+++ ezyfox-server-databridge/src/main/java/com/tvd12/ezyfoxserver/databridge/statistics/EzyNetworkPoint.java
@@ -2,15 +2,29 @@ package com.tvd12.ezyfoxserver.databridge.statistics;
 
 import java.io.Serializable;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.tvd12.ezyfoxserver.statistics.EzyNetworkRoStats;
+import com.tvd12.ezyfoxserver.statistics.EzySocketStatistics;
+import com.tvd12.ezyfoxserver.statistics.EzyStatistics;
+import com.tvd12.ezyfoxserver.statistics.EzyWebSocketStatistics;
 
-@Setter
+import lombok.Getter;
+
 @Getter
 public class EzyNetworkPoint implements Serializable {
 	private static final long serialVersionUID = -6537078366231599664L;
 	
 	private long inputBytes;
 	private long outputBytes;
+	
+	public EzyNetworkPoint(EzyStatistics statistics) {
+		EzySocketStatistics socketStats = statistics.getSocketStats();
+		EzyWebSocketStatistics webSocketStats = statistics.getWebSocketStats();
+		EzyNetworkRoStats socketNetworkStats = socketStats.getNetworkStats();
+		EzyNetworkRoStats webSocketNetworkStats = webSocketStats.getNetworkStats();
+		this.inputBytes = socketNetworkStats.getReadBytesPerSecond()
+				+ webSocketNetworkStats.getReadBytesPerSecond();
+		this.outputBytes = socketNetworkStats.getWrittenBytesPerSecond()
+				+ webSocketNetworkStats.getWrittenBytesPerSecond();
+	}
 	
 }

3d7abf8acb8b4fadc9b47851d6d511004a47a8a6
Dung Ta Van <itprono3@gmail.com>
2019-11-29 23:27:29 +0700

    add unitest

---
+++ ezyfox-server-nio/src/test/java/com/tvd12/ezyfoxserver/nio/testing/EzyNioServerBootstrapTest.java
@@ -0,0 +1,105 @@
+package com.tvd12.ezyfoxserver.nio.testing;
+
+import static org.mockito.Mockito.mock;
+
+import javax.net.ssl.SSLContext;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfoxserver.EzyBootstrap;
+import com.tvd12.ezyfoxserver.EzySimpleServer;
+import com.tvd12.ezyfoxserver.api.EzyResponseApi;
+import com.tvd12.ezyfoxserver.api.EzyStreamingApi;
+import com.tvd12.ezyfoxserver.context.EzyServerContext;
+import com.tvd12.ezyfoxserver.context.EzySimpleServerContext;
+import com.tvd12.ezyfoxserver.controller.EzyServerReadyController;
+import com.tvd12.ezyfoxserver.event.EzyServerReadyEvent;
+import com.tvd12.ezyfoxserver.nio.EzyNioServerBootstrap;
+import com.tvd12.ezyfoxserver.nio.wrapper.EzyHandlerGroupManager;
+import com.tvd12.ezyfoxserver.setting.EzyEventControllersSetting;
+import com.tvd12.ezyfoxserver.setting.EzySimpleEventControllersSetting;
+import com.tvd12.ezyfoxserver.setting.EzySimpleSettings;
+import com.tvd12.ezyfoxserver.setting.EzySimpleStreamingSetting;
+import com.tvd12.ezyfoxserver.socket.EzyBlockingSessionTicketsQueue;
+import com.tvd12.ezyfoxserver.socket.EzyBlockingSocketStreamQueue;
+import com.tvd12.ezyfoxserver.socket.EzySessionTicketsQueue;
+import com.tvd12.ezyfoxserver.socket.EzySimpleSocketRequestQueues;
+import com.tvd12.ezyfoxserver.socket.EzySocketDisconnectionQueue;
+import com.tvd12.ezyfoxserver.socket.EzySocketRequestQueues;
+import com.tvd12.ezyfoxserver.socket.EzySocketStreamQueue;
+import com.tvd12.ezyfoxserver.wrapper.EzyEventControllers;
+import com.tvd12.ezyfoxserver.wrapper.EzyServerControllers;
+import com.tvd12.ezyfoxserver.wrapper.impl.EzyEventControllersImpl;
+import com.tvd12.ezyfoxserver.wrapper.impl.EzyServerControllersImpl;
+import com.tvd12.test.base.BaseTest;
+
+public class EzyNioServerBootstrapTest extends BaseTest {
+
+	@Test
+	public void test() throws Exception {
+		SSLContext sslContext = SSLContext.getDefault();
+		EzyResponseApi responseApi = mock(EzyResponseApi.class);
+		EzyStreamingApi streamingApi = mock(EzyStreamingApi.class);
+		EzySocketRequestQueues requestQueues = new EzySimpleSocketRequestQueues();
+		EzySocketStreamQueue streamQueue = new EzyBlockingSocketStreamQueue();
+		EzyHandlerGroupManager handlerGroupManager = mock(EzyHandlerGroupManager.class);
+		EzySessionTicketsQueue socketSessionTicketsQueue = new EzyBlockingSessionTicketsQueue();
+		EzySessionTicketsQueue websocketSessionTicketsQueue = new EzyBlockingSessionTicketsQueue();
+		EzySocketDisconnectionQueue socketDisconnectionQueue = mock(EzySocketDisconnectionQueue.class);
+		
+		EzySimpleSettings settings = new EzySimpleSettings();
+		EzySimpleStreamingSetting streaming = settings.getStreaming();
+		streaming.setEnable(true);
+		EzySimpleServer server = new EzySimpleServer();
+		EzyServerControllers serverControllers = EzyServerControllersImpl.builder().build();
+		server.setControllers(serverControllers);
+		EzyEventControllersSetting eventControllersSetting = new EzySimpleEventControllersSetting();
+		EzyEventControllers eventControllers = EzyEventControllersImpl.create(eventControllersSetting);
+		server.setEventControllers(eventControllers);
+		server.setSettings(settings);
+		EzySimpleServerContext serverContext = new EzySimpleServerContext();
+		serverContext.setServer(server);
+		serverContext.init();
+		
+		ExBootstrap localBootstrap = new ExBootstrap(new EzyBootstrap.Builder()
+				.context(serverContext));
+		
+		EzyNioServerBootstrap bootstrap = new EzyNioServerBootstrap();
+		bootstrap.setContext(serverContext);
+		bootstrap.setLocalBootstrap(localBootstrap);
+		bootstrap.setSslContext(sslContext);
+		bootstrap.setResponseApi(responseApi);
+		bootstrap.setStreamingApi(streamingApi);
+		bootstrap.setRequestQueues(requestQueues);
+		bootstrap.setStreamQueue(streamQueue);
+		bootstrap.setHandlerGroupManager(handlerGroupManager);
+		bootstrap.setSocketSessionTicketsQueue(socketSessionTicketsQueue);
+		bootstrap.setWebsocketSessionTicketsQueue(websocketSessionTicketsQueue);
+		bootstrap.setSocketDisconnectionQueue(socketDisconnectionQueue);
+		bootstrap.start();
+		bootstrap.destroy();
+		bootstrap.destroy();
+		
+	}
+	
+	public static class ExBootstrap extends EzyBootstrap {
+
+		protected ExBootstrap(Builder builder) {
+			super(builder);
+		}
+		
+		@Override
+		public void start() throws Exception {
+		}
+		
+	}
+	
+	public static class ExServerReadyController implements EzyServerReadyController {
+
+		@Override
+		public void handle(EzyServerContext ctx, EzyServerReadyEvent event) {
+		}
+		
+	}
+	
+}
---
+++ ezyfox-server-nio/src/test/java/com/tvd12/ezyfoxserver/nio/testing/handler/EzySimpleNioHandlerGroupTest.java
@@ -0,0 +1,121 @@
+package com.tvd12.ezyfoxserver.nio.testing.handler;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Queue;
+import java.util.concurrent.ExecutorService;
+
+import org.testng.annotations.Test;
+
+import com.tvd12.ezyfox.codec.EzyByteToObjectDecoder;
+import com.tvd12.ezyfox.codec.EzyMessage;
+import com.tvd12.ezyfox.codec.EzyMessageHeader;
+import com.tvd12.ezyfox.codec.EzySimpleMessage;
+import com.tvd12.ezyfox.codec.EzySimpleMessageHeader;
+import com.tvd12.ezyfox.concurrent.EzyExecutors;
+import com.tvd12.ezyfox.factory.EzyEntityFactory;
+import com.tvd12.ezyfoxserver.EzySimpleServer;
+import com.tvd12.ezyfoxserver.constant.EzyCommand;
+import com.tvd12.ezyfoxserver.constant.EzyConnectionType;
+import com.tvd12.ezyfoxserver.constant.EzyTransportType;
+import com.tvd12.ezyfoxserver.context.EzySimpleServerContext;
+import com.tvd12.ezyfoxserver.nio.builder.impl.EzyHandlerGroupBuilderFactoryImpl;
+import com.tvd12.ezyfoxserver.nio.entity.EzyNioSession;
+import com.tvd12.ezyfoxserver.nio.factory.EzyHandlerGroupBuilderFactory;
+import com.tvd12.ezyfoxserver.nio.handler.EzySimpleNioHandlerGroup;
+import com.tvd12.ezyfoxserver.nio.wrapper.impl.EzyNioSessionManagerImpl;
+import com.tvd12.ezyfoxserver.service.impl.EzySimpleSessionTokenGenerator;
+import com.tvd12.ezyfoxserver.setting.EzySimpleSettings;
+import com.tvd12.ezyfoxserver.setting.EzySimpleStreamingSetting;
+import com.tvd12.ezyfoxserver.socket.EzyBlockingSessionTicketsQueue;
+import com.tvd12.ezyfoxserver.socket.EzyChannel;
+import com.tvd12.ezyfoxserver.socket.EzySessionTicketsQueue;
+import com.tvd12.ezyfoxserver.socket.EzySimplePacket;
+import com.tvd12.ezyfoxserver.socket.EzySimpleSocketRequestQueues;
+import com.tvd12.ezyfoxserver.socket.EzySocketRequestQueues;
+import com.tvd12.ezyfoxserver.statistics.EzySimpleStatistics;
+import com.tvd12.ezyfoxserver.statistics.EzyStatistics;
+import com.tvd12.ezyfoxserver.wrapper.EzySessionManager;
+import com.tvd12.test.base.BaseTest;
+
+public class EzySimpleNioHandlerGroupTest extends BaseTest {
+
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void test() throws Exception {
+		EzySessionTicketsQueue socketSessionTicketsQueue = new EzyBlockingSessionTicketsQueue();
+		EzySessionTicketsQueue webSocketSessionTicketsQueue = new EzyBlockingSessionTicketsQueue();
+		EzyStatistics statistics = new EzySimpleStatistics();
+		EzyHandlerGroupBuilderFactory handlerGroupBuilderFactory = EzyHandlerGroupBuilderFactoryImpl.builder()
+				.statistics(statistics)
+				.socketSessionTicketsQueue(socketSessionTicketsQueue)
+				.webSocketSessionTicketsQueue(webSocketSessionTicketsQueue)
+				.build();
+		EzySimpleSettings settings = new EzySimpleSettings();
+		EzySimpleStreamingSetting streaming = settings.getStreaming();
+		streaming.setEnable(true);
+		EzySimpleServer server = new EzySimpleServer();
+		server.setSettings(settings);
+		EzySimpleServerContext serverContext = new EzySimpleServerContext();
+		serverContext.setServer(server);
+		serverContext.init();
+		
+		EzySessionManager sessionManager = EzyNioSessionManagerImpl.builder()
+				.tokenGenerator(new EzySimpleSessionTokenGenerator())
+				.build();
+		EzyChannel channel = mock(EzyChannel.class);
+		when(channel.isConnected()).thenReturn(true);
+		when(channel.getConnection()).thenReturn(SocketChannel.open());
+		when(channel.getConnectionType()).thenReturn(EzyConnectionType.SOCKET);
+		EzyNioSession session = (EzyNioSession) sessionManager.provideSession(channel);
+		ExEzyByteToObjectDecoder decoder = new ExEzyByteToObjectDecoder();
+		ExecutorService codecThreadPool = EzyExecutors.newFixedThreadPool(1, "codec");
+		ExecutorService statsThreadPool = EzyExecutors.newFixedThreadPool(1, "stats");
+		EzySocketRequestQueues requestQueues = new EzySimpleSocketRequestQueues();
+		
+		EzySimpleNioHandlerGroup group = (EzySimpleNioHandlerGroup) handlerGroupBuilderFactory.newBuilder(EzyConnectionType.SOCKET)
+				.session(session)
+				.decoder(decoder)
+				.serverContext(serverContext)
+				.codecThreadPool(codecThreadPool)
+				.statsThreadPool(statsThreadPool)
+				.requestQueues(requestQueues)
+				.build();
+		
+		group.fireBytesReceived("hello".getBytes());
+		EzySimplePacket packet = new EzySimplePacket();
+		packet.setBinary(true);
+		packet.setData("world".getBytes());
+		packet.setTransportType(EzyTransportType.TCP);
+		ByteBuffer writeBuffer = ByteBuffer.allocate(1024);
+		group.firePacketSend(packet, writeBuffer);
+		group.sendPacketNow(packet);
+		Thread.sleep(2000);
+	}
+	
+	public static class ExEzyByteToObjectDecoder implements EzyByteToObjectDecoder {
+
+		@Override
+		public void reset() {}
+
+		@Override
+		public Object decode(EzyMessage message) throws Exception {
+			return EzyEntityFactory.newArrayBuilder()
+					.append(EzyCommand.PING.getId())
+					.build();
+		}
+
+		@Override
+		public void decode(ByteBuffer bytes, Queue<EzyMessage> queue) throws Exception {
+			EzyMessageHeader header = new EzySimpleMessageHeader(false, false, false, false, false);
+			byte[] content = new byte[bytes.remaining()];
+			bytes.get(content);
+			EzyMessage message = new EzySimpleMessage(header, content, content.length);
+			queue.add(message);
+		}
+		
+	}
+}

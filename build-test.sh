3ab4196c8addf6401ecd1f6fcc64c1370a2b6d6f
Dung Ta Van <itprono3@gmail.com>
2020-09-06 21:07:38 +0700

    update build script

--- build-test.sh
+++ build-test.sh
@@ -4,3 +4,5 @@ mvn clean install -f ezyfox-server-boot
 mvn clean install -f ezyfox-server-nio
 mvn clean install -f ezyfox-server-niorunner
 mvn clean install -f ezyfox-server-support
+mvn clean install -f ezyfox-server-support
+mvn clean install -f ezyfox-server-embedded
--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ mvn clean install -Dmaven.test.skip=true -f ezyfox-server-boot
 mvn clean install -Dmaven.test.skip=true -f ezyfox-server-nio
 mvn clean install -Dmaven.test.skip=true -f ezyfox-server-niorunner
 mvn clean install -Dmaven.test.skip=true -f ezyfox-server-support
+mvn clean install -Dmaven.test.skip=true -f ezyfox-server-embedded

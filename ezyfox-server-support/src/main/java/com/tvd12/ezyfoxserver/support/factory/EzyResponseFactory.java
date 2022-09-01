56603da7ca8127e211487f8b0c48e894b50fb58a
Dung Ta Van <itprono3@gmail.com>
2022-09-01 08:48:46 +0700

    add java docs

--- ezyfox-server-support/src/main/java/com/tvd12/ezyfoxserver/support/factory/EzyResponseFactory.java
+++ ezyfox-server-support/src/main/java/com/tvd12/ezyfoxserver/support/factory/EzyResponseFactory.java
@@ -3,9 +3,24 @@ package com.tvd12.ezyfoxserver.support.factory;
 import com.tvd12.ezyfoxserver.support.command.EzyArrayResponse;
 import com.tvd12.ezyfoxserver.support.command.EzyObjectResponse;
 
+/**
+ * A factory class to create response command.
+ */
 public interface EzyResponseFactory {
 
+    /**
+     * Create a response command.
+     * This command will respond data to client in array.
+     *
+     * @return the array response command.
+     */
     EzyArrayResponse newArrayResponse();
 
+    /**
+     * Create a response command.
+     * This command will respond to client in object (key - value).
+     *
+     * @return the array response command.
+     */
     EzyObjectResponse newObjectResponse();
 }

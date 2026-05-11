89cd92336c896a47967144886072db038ab42648
tvd12 <ta.van.dung@youngmonkeys.org>
2026-05-11 09:56:08 +0700

    fix handshakeComplete indent

--- ezyfox-server-nio/src/main/java/com/tvd12/ezyfoxserver/nio/socket/EzyNioSecureSocketChannel.java
+++ ezyfox-server-nio/src/main/java/com/tvd12/ezyfoxserver/nio/socket/EzyNioSecureSocketChannel.java
@@ -64,7 +64,7 @@ public class EzyNioSecureSocketChannel
             logger.info(
                 "channel: {} handshake already initiated, handshakeComplete: {}",
                 channel,
-                    handshakeComplete
+                handshakeComplete
             );
             return;
         }

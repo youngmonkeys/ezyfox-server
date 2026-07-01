27b7c4078f59ce4954dbf769b123743debaaa9a2
tvd12 <ta.van.dung@youngmonkeys.org>
2026-07-01 15:16:35 +0700

    update

--- ezyfox-server-support/src/test/java/com/tvd12/ezyfoxserver/support/test/asm/EzyExceptionHandlersImplementerTest.java
+++ ezyfox-server-support/src/test/java/com/tvd12/ezyfoxserver/support/test/asm/EzyExceptionHandlersImplementerTest.java
@@ -5,9 +5,14 @@ import com.tvd12.ezyfox.core.annotation.EzyTryCatch;
 import com.tvd12.ezyfoxserver.entity.EzySession;
 import com.tvd12.ezyfoxserver.support.asm.EzyExceptionHandlerImplementer;
 import com.tvd12.ezyfoxserver.support.asm.EzyExceptionHandlersImplementer;
+import com.tvd12.ezyfoxserver.support.handler.EzyUncaughtExceptionHandler;
+import com.tvd12.ezyfoxserver.support.reflect.EzyExceptionHandlerMethod;
+import com.tvd12.ezyfoxserver.support.reflect.EzyExceptionHandlerProxy;
+import com.tvd12.test.assertion.Asserts;
 import org.testng.annotations.Test;
 
 import java.util.Collections;
+import java.util.Map;
 
 public class EzyExceptionHandlersImplementerTest {
 
@@ -18,6 +23,33 @@ public class EzyExceptionHandlersImplementerTest {
         implementer.implement(Collections.singletonList(new ExExceptionHandler()));
         EzyExceptionHandlerImplementer.setDebug(false);
         implementer.implement(Collections.singletonList(new ExExceptionHandler()));
+    }
+
+    @Test
+    public void implementWithNullHandler() {
+        // given
+        EzyExceptionHandlersImplementer implementer = new EzyExceptionHandlersImplementer() {
+            @Override
+            protected EzyExceptionHandlerImplementer newImplementer(
+                EzyExceptionHandlerProxy exceptionHandler,
+                EzyExceptionHandlerMethod method
+            ) {
+                return new EzyExceptionHandlerImplementer(exceptionHandler, method) {
+                    @Override
+                    public EzyUncaughtExceptionHandler implement() {
+                        return null;
+                    }
+                };
+            }
+        };
+
+        // when
+        Map<Class<?>, EzyUncaughtExceptionHandler> handlers = implementer.implement(
+            Collections.singletonList(new ExExceptionHandler())
+        );
+
+        // then
+        Asserts.assertEquals(handlers.size(), 0);
     }
 
     @EzyExceptionHandler
